361ec04bd307fbbe663f56efb890e394c4a97ca4
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2020-08-22 16:08:53 +0100

    Changed to version 2.0

---
+++ ideologies.js
@@ -0,0 +1,190 @@
+ideology = [
+	{
+		"ideology": "Placeholder",
+		"flag": "noflag.png"
+	},
+	{
+		"ideology": "You're an Agorist",
+		"flag": "flags/agorist_flag.png"
+	},
+	{
+		"ideology": "You're an Agrarian Socialist",
+		"flag": "flags/agsoc_flag.png"
+	},
+	{
+		"ideology": "You're an Anarcho-Capitalist",
+		"flag": "flags/ancap_flag.png"
+	},
+	{
+		"ideology": "You're an Anarcho-Collectivism",
+		"flag": "flags/ancol_flag.png"
+	},
+	{
+		"ideology": "You're an Anarcho-Communist",
+		"flag": "flags/ancom_flag.png"
+	},
+	{
+		"ideology": "You're a Geo-Anarchist",
+		"flag": "flags/angeo_flag.png"
+	},
+	{
+		"ideology": "You're an Anarcho-Primitvism",
+		"flag": "flags/anprim_flag.png"
+	},
+	{
+		"ideology": "You're an Anarcho-Syndicalism",
+		"flag": "flags/ansynd_flag.png"
+	},
+	{
+		"ideology": "You're a Bleeding-Heart Libertarian",
+		"flag": "flags/bhl_flag.png"
+	},
+	{
+		"ideology": "You're a Bordigist/Italian Left communist",
+		"flag": "flags/bordiga_flag.png"
+	},
+	{
+		"ideology": "You're a Castroist",
+		"flag": "flags/castro_flag.png"
+	},
+	{
+		"ideology": "You're a Classical Fascist",
+		"flag": "flags/classfash_flag.png"
+	},
+	{
+		"ideology": "You're a Clerical Fascist",
+		"flag": "flags/clerfash_flag.png"
+	},
+	{
+		"ideology": "You're a Council Communist",
+		"flag": "flags/councom_flag.png"
+	},
+	{
+		"ideology": "You're a De Leonist",
+		"flag": "flags/deleon_flag.png"
+	},
+	{
+		"ideology": "You're a Democratic Socialist",
+		"flag": "flags/demsoc_flag.png"
+	},
+	{
+		"ideology": "You're a Dengist/Socialist with Chinese Characteristics",
+		"flag": "flags/deng_flag.png"
+	},
+	{
+		"ideology": "You're a Georgist",
+		"flag": "flags/geo_flag.png"
+	},
+	{
+		"ideology": "You're a Geolibertarian",
+		"flag": "flags/geolib_flag.png"
+	},
+	{
+		"ideology": "You're a Libertarian Socialist",
+		"flag": "flags/libsoc_flag.png"
+	},
+	{
+		"ideology": "You're a Luxembourgist",
+		"flag": "flags/luxembourg_flag.png"
+	},
+	{
+		"ideology": "You're a Left-Wing Market Anarchist",
+		"flag": "flags/lwma_flag.png"
+	},
+	{
+		"ideology": "You're a Maoist",
+		"flag": "flags/mao_flag.png"
+	},
+	{
+		"ideology": "You're a Market Socialist",
+		"flag": "flags/marksoc_flag.png"
+	},
+	{
+		"ideology": "You're a Minarchist",
+		"flag": "flags/minarch_flag.png"
+	},
+	{
+		"ideology": "You're a Marxist–Leninist–Maoist",
+		"flag": "flags/mlm_flag.png"
+	},
+	{
+		"ideology": "You're a Marxist–Leninist",
+		"flag": "flags/ml_flag.png"
+	},
+	{
+		"ideology": "You're a Mutualist",
+		"flag": "flags/mutualist_flag.png"
+	},
+	{
+		"ideology": "You're a National Anarchist",
+		"flag": "flags/nam_flag.png"
+	},
+	{
+		"ideology": "You're a National Syndicalist",
+		"flag": "flags/natsynd_flag.png"
+	},
+	{
+		"ideology": "You're a National Bolshevik",
+		"flag": "flags/nazbol_flag.png"
+	},
+	{
+		"ideology": "You're a National Capitalist",
+		"flag": "flags/nazcap_flag.png"
+	},
+	{
+		"ideology": "You're a National Socialist",
+		"flag": "flags/nazi_flag.png"
+	},
+	{
+		"ideology": "You're a Neoconservative",
+		"flag": "flags/neocon_flag.png"
+	},
+	{
+		"ideology": "You're a Neoliberal",
+		"flag": "flags/neolib_flag.png"
+	},
+	{
+		"ideology": "You're an Orthodox Marxist",
+		"flag": "flags/orthmarx_flag.png"
+	},
+	{
+		"ideology": "You're a Paleoconservative",
+		"flag": "flags/paleocon_flag.png"
+	},
+	{
+		"ideology": "You're a Posadist",
+		"flag": "flags/posadist_flag.png"
+	},
+	{
+		"ideology": "You're a Situationist",
+		"flag": "flags/situationist_flag.png"
+	},
+	{
+		"ideology": "You're a Social Democrat",
+		"flag": "flags/socdem_flag.png"
+	},
+	{
+		"ideology": "You're a Social Georgist",
+		"flag": "flags/socgeo_flag.png"
+	},
+	{
+		"ideology": "You're a State Socialist",
+		"flag": "flags/statsoc_flag.png"
+	},
+	{
+		"ideology": "You're a Strasserist",
+		"flag": "flags/strasser_flag.png"
+	},
+	{
+		"ideology": "You're a Syndicalist",
+		"flag": "flags/synd_flag.png"
+	},
+	{
+		"ideology": "You're a Titoist",
+		"flag": "flags/tito_flag.png"
+	},
+	{
+		"ideology": "You're a Trotskyist",
+		"flag": "flags/trot_flag.png"
+	},
+];
