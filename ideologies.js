81ba97f604cb92d6ec4286f8c5fc220c47bfe3b6
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2020-12-28 14:30:34 +0000

    Updated flags and CSS
    Replaced flags with SVG versions and updated CSS to include automatic dark mode

--- ideologies.js
+++ ideologies.js
@@ -3,287 +3,287 @@ ideology = [
 	},
 	{
 		"ideology": "You're a Fourth Theorist", //Result 1
-		"flag": "flags/4theory_flag.png",
-		"desc": ""
+		"flag": "flags/4theory_flag.svg",
+		"desc": "The fourth theory is an ideology concieved by the Russian political analyst Aleksandr Dugin that seeks to find a new poltiical theory past Liberalism, Marxism and Fascism."
 	},
 	{
 		"ideology": "You're an Agorist", //Result 2
-		"flag": "flags/agorist_flag.png",
-		"desc": ""
+		"flag": "flags/Agorism_flag.svg",
+		"desc": "Agorism is an ideology concieved by Samuel Edward Konkin III that seeks to establish a society in which all exchanges are voluntary, by the means of engagin in counter-economic activities."
 	},
 	{
 		"ideology": "You're an Agrarian Socialist", //Result 3
-		"flag": "flags/agsoc_flag.png",
-		"desc": ""
+		"flag": "flags/Agsoc_flag.svg",
+		"desc": "Agrarian Socialism is an ideology that seeks to combine an agrarian lifestyle and socialist economic system, mostly focusing on land redistribution and social ownership of land."
 	},
 	{
 		"ideology": "You're an Anarcho-Capitalist", //Result 4
-		"flag": "flags/ancap_flag.png",
-		"desc": ""
+		"flag": "flags/Ancap_flag.svg",
+		"desc": "Anarcho-Capitalism, also called Voluntaryism, is an ideology that advocates for the creation of a stateless society based on property rights and the Non-Agression Priciple."
 	},
 	{
 		"ideology": "You're an Anarcho-Collectivist", //Result 5
-		"flag": "flags/ancol_flag.png",
-		"desc": ""
+		"flag": "flags/Ancol_flag.svg",
+		"desc": "Anarcho-Collectivism is an ideology that seeks to establish a stateless society with worker owned means of production and exchange of goods for labour notes, representative of the labour of the worker."
 	},
 	{
-		"ideology": "You're a Anarcho-Communist", //Result 6
-		"flag": "flags/ancom_flag.png",
-		"desc": ""
+		"ideology": "You're an Anarcho-Communist", //Result 6
+		"flag": "flags/Ancom_flag.svg",
+		"desc": "Anarcho-Communism is an ideology that seeks to establish a stateless, classless and moneyless society based on a mutual aid gift economy."
 	},
 	{
 		"ideology": "You're a Geoanarchist", //Result 7
-		"flag": "flags/angeo_flag.png",
-		"desc": ""
+		"flag": "flags/Angeo_flag.svg",
+		"desc": "Geo-anarchism is an ideology that advocates for the establishment of a stateless society where private ownership of businesses is present but land is commonly owned and its usage is taxed to a mutual community fund."
 	},
 	{
 		"ideology": "You're an Anarcho-Primitivist", //Result 8
-		"flag": "flags/anprim_flag.png",
+		"flag": "flags/Anprim_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're an Anarcho-Syndicalist",  //Result 9
-		"flag": "flags/ansynd_flag.png",
+		"flag": "flags/Ansynd_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Bleeding-Heart Libertarian", //Result 10
-		"flag": "flags/bhl_flag.png",
-		"desc": ""
+		"flag": "flags/Bhl_flag.svg",
+		"desc": "Bleeding-Heart Libertarianism is an ideology that deems that an expansion of civil liberties will be the most benefitial to the disadvanatged"
 	},
 	{
 		"ideology": "You're a Bordigist/Italian Left-Communist", //Result 11
-		"flag": "flags/bordiga_flag.png",
+		"flag": "flags/Bordiga_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Castroist", //Result 12
-		"flag": "flags/castro_flag.png",
+		"flag": "flags/Castro_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Classical Fascist", //Result 13
-		"flag": "flags/classfash_flag.png",
+		"flag": "flags/Fascism_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Clerical Fascist", //Result 14
-		"flag": "flags/clerfash_flag.png",
+		"flag": "flags/Clerfash_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Council Communist", //Result 15
-		"flag": "flags/councom_flag.png",
+		"flag": "flags/Councom_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a De Leonist", //Result 16
-		"flag": "flags/deleon_flag.png",
+		"flag": "flags/DeLeon_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Democatic Socialist", //Result 17
-		"flag": "flags/demsoc_flag.png",
+		"flag": "flags/Demsoc_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Dengist/Socialist with Chinese Characteristics", //Result 18
-		"flag": "flags/deng_flag.png",
+		"flag": "flags/Deng_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Distributist", //Result 19
-		"flag": "flags/dist_flag.png",
+		"flag": "flags/Distributism_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Geolibertarian", //Result 20
-		"flag": "flags/geolib_flag.png",
+		"flag": "flags/Geolib_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Georgist", //Result 21
-		"flag": "flags/geo_flag.png",
+		"flag": "flags/Georgism_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Left-Rothbardian", //Result 22
-		"flag": "flags/leftroth_flag.png",
+		"flag": "flags/Leftroth_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Right-Libertarian", //Result 23
-		"flag": "flags/libert_flag.png",
+		"flag": "flags/Libert_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Libertarian Socialist", //Result 24
-		"flag": "flags/libsoc_flag.png",
+		"flag": "flags/Libsoc_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Keynesian Liberal",//Result 25
-		"flag": "flags/lib_flag.png",
+		"flag": "flags/Lib_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Luxemburgist",//Result 26
-		"flag": "flags/luxem_flag.png",
+		"flag": "flags/Luxem_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Left-Wing Market Anarchist", //Result 27
-		"flag": "flags/lwma_flag.png",
+		"flag": "flags/Lwma_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Maoist", //Result 28
-		"flag": "flags/mao_flag.png",
+		"flag": "flags/Mao_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Market Socialist", //Result 29
-		"flag": "flags/marksoc_flag.png",
+		"flag": "flags/Marksoc_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Minarchist", //Result 30
-		"flag": "flags/minarch_flag.png",
+		"flag": "flags/Minarch_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Marxist–Leninist–Maoist", //Result 31
-		"flag": "flags/mlm_flag.png",
+		"flag": "flags/MLM_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Marxist–Leninist", //Result 32
-		"flag": "flags/ml_flag.png",
+		"flag": "flags/ML_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Mutualist", //Result 33
-		"flag": "flags/mutualist_flag.png",
+		"flag": "flags/Mutualism_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a National Anarchist", //Result 34
-		"flag": "flags/nam_flag.png",
+		"flag": "flags/Natan_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a National Syndicalist", //Result 35
-		"flag": "flags/natsynd_flag.png",
+		"flag": "flags/Natsynd_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a National Bolshevik", //Result 36
-		"flag": "flags/nazbol_flag.png",
+		"flag": "flags/Nazbol_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a National Capitalist", //Result 37
-		"flag": "flags/nazcap_flag.png",
+		"flag": "flags/Nazcap_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a National Socialist", //Result 38
-		"flag": "flags/nazi_flag.png",
+		"flag": "flags/Nazi_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Neoconservative", //Result 39
-		"flag": "flags/neocon_flag.png",
+		"flag": "flags/Neocon_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Neoliberal", //Result 40
-		"flag": "flags/neolib_flag.png",
+		"flag": "flags/Neolib_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're an Orthodox Marxist", //Result 41
-		"flag": "flags/orthmarx_flag.png",
+		"flag": "flags/Orthmarx_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Paleolibertarian", //Result 42
-		"flag": "flags/paleobert_flag.png",
+		"flag": "flags/Paleobert_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Paleoonservative", //Result 43
-		"flag": "flags/paleocon_flag.png",
+		"flag": "flags/Paleocon_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Paternalistic Conservative", //Result 44
-		"flag": "flags/patcon_flag.png",
+		"flag": "flags/Patcon_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Posadist", //Result 45
-		"flag": "flags/posadist_flag.png",
+		"flag": "flags/Posadism_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Situationist", //Result 46
-		"flag": "flags/situationist_flag.png",
+		"flag": "flags/Situationist_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Social Libertarian", //Result 47
-		"flag": "flags/socbert_flag.png",
+		"flag": "flags/Socbert_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Social Democrat", //Result 48
-		"flag": "flags/socdem_flag.png",
+		"flag": "flags/Socdem_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Social Georgist", //Result 49
-		"flag": "flags/socgeo_flag.png",
+		"flag": "flags/Socgeo_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Social Nationalist", //Result 50
-		"flag": "flags/socnat_flag.png",
+		"flag": "flags/Socnat_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a State Socialist", //Result 51
-		"flag": "flags/statesoc_flag.png",
+		"flag": "flags/Statesoc_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Strasserist", //Result 52
-		"flag": "flags/strasser_flag.png",
+		"flag": "flags/Strasser_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Syndicalist", //Result 53
-		"flag": "flags/synd_flag.png",
+		"flag": "flags/Synd_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Technocrat", //Result 54
-		"flag": "flags/technocracy_flag.png",
-		"desc": ""
+		"flag": "flags/Technocracy_flag.svg",
+		"desc": "Technocracy is an ideology that deems that the state and the economy should be ran by experts of their given field as opposed to elected/inherited politicians and businessmen"
 	},
 	{
 		"ideology": "You're a Theocrat", //Result 55
-		"flag": "flags/theocracy_flag.png",
-		"desc": ""
+		"flag": "flags/Theocracy_flag.svg",
+		"desc": "A theocracy is an ideology where the rulers are meant to act as representatives of a deity and enforce the laws of their given religion upon the citizens"
 	},
 	{
 		"ideology": "You're a Titoist", //Result 56
-		"flag": "flags/tito_flag.png",
+		"flag": "flags/Tito_flag.svg",
 		"desc": ""
 	},
 	{
 		"ideology": "You're a Trotskyist", //Result 57
-		"flag": "flags/trot_flag.png",
+		"flag": "flags/Trot_flag.svg",
 		"desc": ""
 	}
 ];
