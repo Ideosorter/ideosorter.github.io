25e81e7dd4bfd61c31040d993653c7933acf36a6
ConarCZ <55970818+ConarCZ@users.noreply.github.com>
2021-03-20 21:22:16 +0000

    basically redid the whole thing

--- ideologies.js
+++ ideologies.js
@@ -1,289 +1,231 @@
-ideology = [
-	{ //Placeholder
-	},
-	{
-		"ideology": "You're a Fourth Theorist", //Result 1
-		"flag": "flags/4theory_flag.svg",
+ideology = {
+	"placeholder": {},
+	"4theory": {
+		"ideology": "You're a Fourth Theorist",
 		"desc": "The fourth theory is an ideology concieved by the Russian political analyst Aleksandr Dugin that seeks to find a new poltiical theory past Liberalism, Marxism and Fascism."
 	},
-	{
+	"agorism": {
 		"ideology": "You're an Agorist", //Result 2
-		"flag": "flags/Agorism_flag.svg",
 		"desc": "Agorism is an ideology concieved by Samuel Edward Konkin III that seeks to establish a society in which all exchanges are voluntary, by the means of engaging in counter-economic activities."
 	},
-	{
+	"agsoc": {
 		"ideology": "You're an Agrarian Socialist", //Result 3
-		"flag": "flags/Agsoc_flag.svg",
 		"desc": "Agrarian Socialism is an ideology that seeks to combine an agrarian lifestyle and socialist economic system, mostly focusing on land redistribution and social ownership of land."
 	},
-	{
+	"ancap": {
 		"ideology": "You're an Anarcho-Capitalist", //Result 4
-		"flag": "flags/Ancap_flag.svg",
 		"desc": "Anarcho-Capitalism, also called Voluntaryism, is an ideology that advocates for the creation of a stateless society based on property rights and the Non-Aggression Principle."
 	},
-	{
+	"ancol": {
 		"ideology": "You're an Anarcho-Collectivist", //Result 5
-		"flag": "flags/Ancol_flag.svg",
 		"desc": "Anarcho-Collectivism is an ideology that seeks to establish a stateless society with worker owned means of production and exchange of goods for labour notes, representative of the labour of the worker."
 	},
-	{
+	"ancom": {
 		"ideology": "You're an Anarcho-Communist", //Result 6
-		"flag": "flags/Ancom_flag.svg",
 		"desc": "Anarcho-Communism is an ideology that seeks to establish a stateless, classless and moneyless society based on a mutual aid gift economy."
 	},
-	{
+	"angeo": {
 		"ideology": "You're a Geoanarchist", //Result 7
-		"flag": "flags/Angeo_flag.svg",
 		"desc": "Geo-anarchism is an ideology that advocates for the establishment of a stateless society where private ownership of businesses is present but land is commonly owned and its usage is taxed to a mutual community fund."
 	},
-	{
+	"anprim": {
 		"ideology": "You're an Anarcho-Primitivist", //Result 8
-		"flag": "flags/Anprim_flag.svg",
 		"desc": "Anarcho-Primitivism is an ideology that advocates for a return to pre-civilization life. It is unique in its opposition to agrculture and pecuary and the embracing of a hunter-gatherer lifestyle"
 	},
-	{
+	"ansynd": {
 		"ideology": "You're an Anarcho-Syndicalist",  //Result 9
-		"flag": "flags/Ansynd_flag.svg",
 		"desc": "Anarcho-Syndicalism is an ideology that seeks to use the power of workers' movements to establish a post-capitalist stateless society based on worker ownership of the means of production"
 	},
-	{
+	"bhl": {
 		"ideology": "You're a Bleeding-Heart Libertarian", //Result 10
-		"flag": "flags/BHL_flag.svg",
 		"desc": "Bleeding-Heart Libertarianism is an ideology that deems that an expansion of civil liberties will be the most benefitial to the disadvanatged"
 	},
-	{
+	"bordiga": {
 		"ideology": "You're a Bordigist/Italian Left-Communist", //Result 11
-		"flag": "flags/Bordiga_flag.svg",
 		"desc": "Italian Left Communism is an ideology that deems that "
 	},
-	{
+	"castro": {
 		"ideology": "You're a Castroist", //Result 12
-		"flag": "flags/Castro_flag.svg",
 		"desc": "Castroism is an ideology "
 	},
-	{
+	"fascism": {
 		"ideology": "You're a Classical Fascist", //Result 13
-		"flag": "flags/Fascism_flag.svg",
 		"desc": ""
 	},
-	{
+	"clerfash": {
 		"ideology": "You're a Clerical Fascist", //Result 14
-		"flag": "flags/Clerfash_flag.svg",
 		"desc": ""
 	},
-	{
+	"councom": {
 		"ideology": "You're a Council Communist", //Result 15
-		"flag": "flags/Councom_flag.svg",
 		"desc": "Council Communism is an ideology that deems that a sucessful socialist society must be one managed by workers' councils and not "
 	},
-	{
+	"deleon": {
 		"ideology": "You're a De Leonist", //Result 16
-		"flag": "flags/DeLeon_flag.svg",
 		"desc": ""
 	},
-	{
+	"demsoc": {
 		"ideology": "You're a Democatic Socialist", //Result 17
-		"flag": "flags/Demsoc_flag.svg",
 		"desc": "Democratic Socialism is an ideology that seeks to establish socialism through reform and electoral processes as opposed to revoluitionary means"
 	},
-	{
+	"deng": {
 		"ideology": "You're a Dengist/Socialist with Chinese Characteristics", //Result 18
-		"flag": "flags/Deng_flag.svg",
 		"desc": "Socialism with Chinese Characteristics is an ideology that deems that a stage of state managed capitalism is necessary to create the conditions necessary for a sucessful socialist society"
 	},
-	{
+	"distributism": {
 		"ideology": "You're a Distributist", //Result 19
-		"flag": "flags/Distributism_flag.svg",
 		"desc": ""
 	},
-	{
+	"geolib": {
 		"ideology": "You're a Geolibertarian", //Result 20
-		"flag": "flags/Geolib_flag.svg",
 		"desc": ""
 	},
-	{
+	"georgism": {
 		"ideology": "You're a Georgist", //Result 21
-		"flag": "flags/Georgism_flag.svg",
 		"desc": "Georgism is an ideology that deems that for a truly free market to exist first the monopoly on land must be broken. It advocates for a land value tax that if implemented properly will in essence decomofify land ownership"
 	},
-	{
+	"leftroth": {
 		"ideology": "You're a Left-Rothbardian", //Result 22
-		"flag": "flags/Leftroth_flag.svg",
 		"desc": ""
 	},
-	{
+	"libert": {
 		"ideology": "You're a Right-Libertarian", //Result 23
-		"flag": "flags/Libert_flag.svg",
 		"desc": ""
 	},
-	{
+	"libsoc": {
 		"ideology": "You're a Libertarian Socialist", //Result 24
-		"flag": "flags/Libsoc_flag.svg",
 		"desc": "Libertarian Socialism is an ideology that deems that the means of production should be owned and managed by their workers with the state providing little to no interference in the economy, contrasted to the ideas of state ownership of State Socialism"
 	},
-	{
+	"lib": {
 		"ideology": "You're a Keynesian Liberal",//Result 25
-		"flag": "flags/Lib_flag.svg",
 		"desc": ""
 	},
-	{
+	"luxem": {
 		"ideology": "You're a Luxemburgist",//Result 26
-		"flag": "flags/Luxem_flag.svg",
 		"desc": ""
 	},
-	{
+	"lwma": {
 		"ideology": "You're a Left-Wing Market Anarchist", //Result 27
-		"flag": "flags/Lwma_flag.svg",
 		"desc": ""
 	},
-	{
+	"mao": {
 		"ideology": "You're a Maoist", //Result 28
-		"flag": "flags/Mao_flag.svg",
 		"desc": ""
 	},
-	{
+	"marksoc": {
 		"ideology": "You're a Market Socialist", //Result 29
-		"flag": "flags/Marksoc_flag.svg",
 		"desc": "Market socialism is an ideology that combines the socialist ideal of worker ownership of the means of production with the competition and resource allocation provided by a market economy"
 	},
-	{
+	"minarch": {
 		"ideology": "You're a Minarchist", //Result 30
-		"flag": "flags/Minarch_flag.svg",
 		"desc": "Minarchism is an ideology that deems that the state's sole purposes are to protect its citizens' rights to property, bring criminals to justice and to guard the country's territory against any invasions."
 	},
-	{
+	"mlm": {
 		"ideology": "You're a Marxist–Leninist–Maoist", //Result 31
-		"flag": "flags/MLM_flag.svg",
 		"desc": "Marxism–Leninism–Maoism is an ideology devised by the Communist Party of Peru that synthesizes the ideas of Marxism–Leninism and Maoism, rejecting certain aspects from the latter like the 3 worlds theory."
 	},
-	{
+	"ml": {
 		"ideology": "You're a Marxist–Leninist", //Result 32
-		"flag": "flags/ML_flag.svg",
 		"desc": "Marxism–Leninism is an ideology that deems that establishing and developing socialism in 1 country is a necessity before exporting the revoltion to neighbouring states, an idea that contrasts with the Trotskyist ideal of permanent revolution."
 	},
-	{
+	"mutualism": {
 		"ideology": "You're a Mutualist", //Result 33
-		"flag": "flags/Mutualism_flag.svg",
 		"desc": "Mutualism is an ideology who seeks to establish a stateless society based around a combination of free trade of goods between individuals, as long as a mutual credit bank and mutual aid structures."
 	},
-	{
+	"natan": {
 		"ideology": "You're a National Anarchist", //Result 34
-		"flag": "flags/Natan_flag.svg",
 		"desc": "National Anarchism is an ideology that seeks to establish a stateless society made up of ethno-communes based on a meritocratic mutualist economic system."
 	},
-	{
+	"natsynd": {
 		"ideology": "You're a National Syndicalist", //Result 35
-		"flag": "flags/Natsynd_flag.svg",
 		"desc": ""
 	},
-	{
+	"nazbol": {
 		"ideology": "You're a National Bolshevik", //Result 36
-		"flag": "flags/Nazbol_flag.svg",
 		"desc": ""
 	},
-	{
+	"nazcap": {
 		"ideology": "You're a National Capitalist", //Result 37
-		"flag": "flags/Nazcap_flag.svg",
 		"desc": ""
 	},
-	{
+	"nazi": {
 		"ideology": "You're a National Socialist", //Result 38
-		"flag": "flags/Nazi_flag.svg",
 		"desc": ""
 	},
-	{
+	"neocon": {
 		"ideology": "You're a Neoconservative", //Result 39
-		"flag": "flags/Neocon_flag.svg",
 		"desc": ""
 	},
-	{
+	"neolib": {
 		"ideology": "You're a Neoliberal", //Result 40
-		"flag": "flags/Neolib_flag.svg",
 		"desc": ""
 	},
-	{
+	"orthmarx": {
 		"ideology": "You're an Orthodox Marxist", //Result 41
-		"flag": "flags/Orthmarx_flag.svg",
 		"desc": ""
 	},
-	{
+	"paleobert": {
 		"ideology": "You're a Paleolibertarian", //Result 42
-		"flag": "flags/Paleobert_flag.svg",
 		"desc": ""
 	},
-	{
+	"paleocon": {
 		"ideology": "You're a Paleoonservative", //Result 43
-		"flag": "flags/Paleocon_flag.svg",
 		"desc": "Paleoconservatism is a form of conservatism that orignally manifestated itself in the isolationist factions of American conservatism who opposed the Vietnam war. Paleoconservatives also commonly oppose free global trade, taking a very protectionist approach with strong tarrifs."
 	},
-	{
+	"patcon": {
 		"ideology": "You're a Paternalistic Conservative", //Result 44
-		"flag": "flags/Patcon_flag.svg",
 		"desc": "Paternalistic conservatism is an ideolody that seeks to combine the social democratic principles of regulated capitalism and strong labour unions with traditional values."
 	},
-	{
+	"posadism": {
 		"ideology": "You're a Posadist", //Result 45
-		"flag": "flags/Posadism_flag.svg",
 		"desc": "Posadism is a radically accelerationist form of Trostksyism that deems nuclear destruction of captalist society as a necessity to establish socialism globally (with some help of our alien comrades, of course)"
 	},
-	{
+	"situationist": {
 		"ideology": "You're a Situationist", //Result 46
-		"flag": "flags/Situationist_flag.svg",
 		"desc": ""
 	},
-	{
+	"socbert": {
 		"ideology": "You're a Social Libertarian", //Result 47
-		"flag": "flags/Socbert_flag.svg",
 		"desc": "Social Libertarianism is a form of Bleeding-Heart Libertarianism that deems that a universal basic income, combined with maximum individual freedoms, is a necessity to achieve a fair society for all including the most socially disadvantaged"
 	},
-	{
+	"socdem": {
 		"ideology": "You're a Social Democrat", //Result 48
-		"flag": "flags/Socdem_flag.svg",
 		"desc": "Social Democracy is an ideology that seeks to establish a society whereby capitalism is restricted and controlled by the state and strong labour unions in order to achieve bennefitial outcomes to the working class."
 	},
-	{
+	"socgeo": {
 		"ideology": "You're a Social Georgist", //Result 49
-		"flag": "flags/Socgeo_flag.svg",
 		"desc": "Social Georgism is an ideology that deems that Georgist principles of public land ownership and land value tax prefectly complement a free market welfarist society, the most prominent advocate of this ideology is Chinese revolutionary and first president of the Republic of China, Sun Yat-Sen"
 	},
-	{
+	"socnat": {
 		"ideology": "You're a Social Nationalist", //Result 50
-		"flag": "flags/Socnat_flag.svg",
 		"desc": ""
 	},
-	{
+	"statesoc": {
 		"ideology": "You're a State Socialist", //Result 51
-		"flag": "flags/Statesoc_flag.svg",
 		"desc": "State Socialism is an ideology that deems that the means of production should be owned and managed by the state, contrasted to the ideals of Libertarian Socialism"
 	},
-	{
+	"strasser": {
 		"ideology": "You're a Strasserist", //Result 52
-		"flag": "flags/Strasser_flag.svg",
 		"desc": "Strasserism is an ideology popularized by the Strasser brothers that seeks to blend left wing economics with right wing ultrantionalism."
 	},
-	{
+	"synd": {
 		"ideology": "You're a Syndicalist", //Result 53
-		"flag": "flags/Synd_flag.svg",
 		"desc": ""
 	},
-	{
+	"technocracy": {
 		"ideology": "You're a Technocrat", //Result 54
-		"flag": "flags/Technocracy_flag.svg",
 		"desc": "Technocracy is an ideology that deems that the state and the economy should be ran by experts of their given field as opposed to elected/inherited politicians and businessmen"
 	},
-	{
+	"theocracy": {
 		"ideology": "You're a Theocrat", //Result 55
-		"flag": "flags/Theocracy_flag.svg",
 		"desc": "A theocracy is an ideology where the rulers are meant to act as representatives of a deity and enforce the laws of their given religion upon the citizens"
 	},
-	{
+	"tito": {
 		"ideology": "You're a Titoist", //Result 56
-		"flag": "flags/Tito_flag.svg",
 		"desc": "Titoism is an ideology that seeks to combine the vanguardist nature of Leninism with a market socialist economy and worker owned means of production"
 	},
-	{
+	"trot": {
 		"ideology": "You're a Trotskyist", //Result 57
-		"flag": "flags/Trot_flag.svg",
 		"desc": "Trotskyism is an ideology that deems a permanent revoltuion across the world to be a necessity in establishing socialism, contrasting to the Marxist–Leninist idea of socialism in 1 country"
 	}
-];
+}
--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
-questionNumber = 0
+questionId = Object.keys(questions)[0]
 init_question()
 function init_question() {
-    document.getElementById("question-text").innerHTML = questions[questionNumber].question
+    document.getElementById("question-text").innerHTML = questions[questionId].question
     // hides everything
     document.getElementById("Answer1").style.display = "none"
     document.getElementById("Answer2").style.display = "none"
@@ -19,73 +19,50 @@ function init_question() {
     document.getElementById("Answer14").style.display = "none"
     document.getElementById("Answer15").style.display = "none"
     document.getElementById("Answer16").style.display = "none"
-    switch (questions[questionNumber].answers) { // todo fix the fucking numbers holy shit
-        case "1": // Answer 1 (Yes) and Answer 2 (No) are displayed
+    switch (questions[questionId].answers) {
+        case 1: // Answer 1 (Yes) and Answer 2 (No) are displayed
             document.getElementById("Answer1").style.display = "block"
             document.getElementById("Answer2").style.display = "block"
             break
-        case "2": // Answer 1 (Yes), Answer 2 (No) and Answer 3 (The state shouldn"t exist) are displayed
+        case 2: // Answer 1 (Yes), Answer 2 (No) and Answer 3 (The state shouldn"t exist) are displayed
             document.getElementById("Answer1").style.display = "block"
             document.getElementById("Answer2").style.display = "block"
             document.getElementById("Answer3").style.display = "block"
             break
-        case "3": // Answer 4 (Conditional) and Answer 5 (Unconditional) are displayed
+        case 3: // Answer 4 (Conditional) and Answer 5 (Unconditional) are displayed
             document.getElementById("Answer4").style.display = "block"
             document.getElementById("Answer5").style.display = "block"
             break
-        case "4": // Answer 6 (Capitalism) and Answer 7 (Corporatism) are displayed
+        case 4: // Answer 6 (Capitalism) and Answer 7 (Corporatism) are displayed
             document.getElementById("Answer6").style.display = "block"
             document.getElementById("Answer7").style.display = "block"
             break
-        case "5": // Answer 8 (Workers) and Answer 10 (State) are displayed
+        case 5: // Answer 8 (Workers) and Answer 10 (State) are displayed
             document.getElementById("Answer8").style.display = "block"
             document.getElementById("Answer10").style.display = "block"
             break
-        case "6": // Answer 9 (Unions) and Answer 10 (State) are displayed
+        case 6: // Answer 9 (Unions) and Answer 10 (State) are displayed
             document.getElementById("Answer9").style.display = "block"
             document.getElementById("Answer10").style.display = "block"
             break
-        case "7": // Answer 11 (Electoralism) and Answer 12 (Revolution) are displayed
+        case 7: // Answer 11 (Electoralism) and Answer 12 (Revolution) are displayed
             document.getElementById("Answer11").style.display = "block"
             document.getElementById("Answer12").style.display = "block"
             break
-        case "8": // Answer 13 (Welfare) and Answer 14 (Not needed) are displayed
+        case 8: // Answer 13 (Welfare) and Answer 14 (Not needed) are displayed
             document.getElementById("Answer13").style.display = "block"
             document.getElementById("Answer14").style.display = "block"
             break
-        case "9": // Answer 15 (Dasein) and Answer 16 (Class/Nation) are displayed
+        case 9: // Answer 15 (Dasein) and Answer 16 (Class/Nation) are displayed
             document.getElementById("Answer15").style.display = "block"
             document.getElementById("Answer16").style.display = "block"
     }
 }
 function next_question(indx) {
-    switch(indx) {
-        // gets the right answer
-        // todo turn it into arrays perhaps?
-        // so that we don't have to do this perhaps?
-        case 1:
-            if (questions[questionNumber].results.a == 0) {
-                questionNumber = questions[questionNumber].answer.a
-                init_question()
-            } else {
-                location.href = "results.html" + "?=" + questions[questionNumber].results.a
-            }
-            break
-        case 2:
-            if (questions[questionNumber].results.b == 0) {
-                questionNumber = questions[questionNumber].answer.b
-                init_question()
-            } else {
-                location.href = "results.html" + "?=" + questions[questionNumber].results.b
-            }
-            break
-        case 3:
-            if (questions[questionNumber].results.c == 0) {
-                questionNumber = questions[questionNumber].answer.c
-                init_question()
-            } else {
-                location.href = "results.html" + "?=" + questions[questionNumber].results.c
-            }
-
+    if (questions[questionId].results[indx] == null) {
+        questionId = questions[questionId].answer[indx]
+        init_question()
+    } else {
+        location.href = "results.html" + "?=" + questions[questionId].results[indx]
     }
 }
