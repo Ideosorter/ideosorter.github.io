5ac355121ba8beb6f8eb4414b19d6070b211d1e4
ConarCZ <ezioproductions20@gmail.com>
2021-03-23 00:17:06 +0000

    fixed the guy without left arm

--- scripts/json/loc.js
+++ scripts/json/loc.js
@@ -4,7 +4,7 @@ localisation = {
         "title": "Ideosorter",
         // Index
         "index_startbutton": "Click here to start!",
-        "index_text": "<h2>What is Ideosorter?</h2><p>Ideosorter is, in essence, a political quiz that attempts to assign an ideology that matches your views the closest by asking questions to you based on a flowchart model. This test will sort you into one of 57 different ideologies based on your answers to these questions; maybe you'll discover something you didn't know about yourself!</p><h2>I don't understand one of the questions!</h2><p>If there's anything you don't understand, we've tried to make any terminology easily looked up, and if it's still not clear, tell us so we can make some changes to the questions!</p><h2>I don't like my matched ideology!</h2><p>¯\_(ツ)_/¯</p>",
+        "index_text": "<h2>What is Ideosorter?</h2><p>Ideosorter is, in essence, a political quiz that attempts to assign an ideology that matches your views the closest by asking questions to you based on a flowchart model. This test will sort you into one of 57 different ideologies based on your answers to these questions; maybe you'll discover something you didn't know about yourself!</p><h2>I don't understand one of the questions!</h2><p>If there's anything you don't understand, we've tried to make any terminology easily looked up, and if it's still not clear, tell us so we can make some changes to the questions!</p><h2>I don't like my matched ideology!</h2><p>¯\\_(ツ)_/¯</p>",
         "index_contacts_text": "Contacts:",
         "index_contacts": "<p>TheGhostOfInky#9229</p><p>theghostofinky@gmail.com</p><p><a href=\"https://github.com/Ideosorter/ideosorter.github.io\">GitHub Page</a></p>", // backslash escapes " meaning you can write " in string
         // Instructions
@@ -17,7 +17,7 @@ localisation = {
         "name": "Čeština (Not finished)",
         "title": "Ideosorter",
         "index_startbutton": "Začněte kliknutím sem!",
-        "index_text": "<h2>Co je Ideosorter?</h2><p>Ideosorter je v podstatě politický kvíz, který se pokouší přiřadit ideologii, která se nejvice schoduje s vašimi nazory pokladaním dotazů založených na vývojovém diagramu. Tento test vás zařadí do jedné z 57 ideologiích podle vaších odpovědí. Možná o sobě objevíte něco co jste nevěděli!</p><h2>Nerozumím jedné z otázek!</h2><p>Pokud něčemu nerozumíte, pokusily jsem se použit snadno dohledatelnou terminologii, a pokud je něco pořád nejasné, dejte nám vědět abychom to mohlie napravit!</p><h2>Nelíbí se mi moje přiřazená ideologie!</h2><p>¯\_(ツ)_/¯</p>",
+        "index_text": "<h2>Co je Ideosorter?</h2><p>Ideosorter je v podstatě politický kvíz, který se pokouší přiřadit ideologii, která se nejvice schoduje s vašimi nazory pokladaním dotazů založených na vývojovém diagramu. Tento test vás zařadí do jedné z 57 ideologiích podle vaších odpovědí. Možná o sobě objevíte něco co jste nevěděli!</p><h2>Nerozumím jedné z otázek!</h2><p>Pokud něčemu nerozumíte, pokusily jsem se použit snadno dohledatelnou terminologii, a pokud je něco pořád nejasné, dejte nám vědět abychom to mohlie napravit!</p><h2>Nelíbí se mi moje přiřazená ideologie!</h2><p>¯\\_(ツ)_/¯</p>",
         "index_contacts_text": "Kontakty:",
         "index_contacts": "<p>TheGhostOfInky#9229</p><p>theghostofinky@gmail.com</p><p><a href=\"https://github.com/Ideosorter/ideosorter.github.io\">Stránka na Githubu</a></p>",
         "instructions_title": "Instrukce",
