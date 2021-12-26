58fa1278d0489e5eb8f05cd1982b81610700628b
TheGhostOfInky <theghostofinky@gmail.com>
2021-12-26 17:32:22 +0000

    Merged dev branch with master

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Ideosorter
-Ideosorter is a flowchart based interactive political quiz.
+Ideosorter is a flowchart based interactive political quiz that attempts to bring a different approach to political quizzes.
+
+## Ideosorter 3 requirements
+ - A browser that supports ECMA2017, including promises, async/await and the [fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) (most browsers released after mid-2017)
+ - **Back button requirements**:
+  - A browser that supports the [Array.prototype.at()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/at) method, currently supported by the latest version of all major browsers but Safari (Currently only available in TP).
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,18 +1,77 @@
-questionId = Object.keys(questions)[0]
-init_question()
+var lang
+var questions
+var questionId
+var buttons
+var answerpath = []
+
+function next_question(sel) {
+    if (questions[questionId].results[sel] == null) {
+        answerpath.push(questionId)
+        questionId = questions[questionId].nextquestion[sel]
+        init_question()
+    } else {
+        location.href = `results.html?${lang}&${questions[questionId].results[sel]}`
+    }
+}
+
+function prev_question(){
+    if(answerpath.length == 0){
+        location.href = `index.html?${lang}`
+    } else {
+        questionId = answerpath.at(-1)
+        answerpath.pop()
+        init_question()
+    }
+}
+
 function init_question() {
-    document.getElementById("question-text").innerHTML = questions[questionId]["question_" + lang]
-    var buttonHTML = ""
-    for(var i = 0; i<questions[questionId].answers.length; i++) {
-        buttonHTML += "<button class=\"button\" onclick=\"next_question(\'" + questions[questionId].answers[i] + "\')\" style=\"background-color: " + buttons[questions[questionId].answers[i]].color + ";\" id=\"Answer1\">" + buttons[questions[questionId].answers[i]]["text_" + lang] + "</button><br>"
+    document.getElementById("question").innerHTML = questions[questionId]["question"]
+    let buttonHTML = ""
+    let answers = questions[questionId].answers
+    for(let i = 0; i<answers.length; i++) {
+        let button = buttons[answers[i]]
+        buttonHTML += `<button class="button" onclick="next_question('${answers[i]}')" style="background-color:${button.bgcolor}; color:${button.textcolor};">${button.text}</button>`
     }
     document.getElementById("buttonholder").innerHTML = buttonHTML
 }
-function next_question(indx) {
-    if (questions[questionId].results[indx] == null) {
-        questionId = questions[questionId].nextquestion[indx]
-        init_question()
-    } else {
-        location.href = "results.html" + "?ideo=" + questions[questionId].results[indx] + "&lang=" + lang
-    }
+
+function load_questions(data){
+    questions = data
+    questionId = Object.keys(data)[0]
+    init_question()
 }
+
+async function load_ui(quiz){
+    document.getElementById("quiz_title").innerHTML = quiz.title
+    if(Array.prototype.at){
+        document.getElementById("back_button").innerHTML = quiz.back
+    } else {
+        document.getElementById("back_button").style.display = "none"
+    }
+    await fetch(`./json/${lang}/buttons-${lang}.json`)
+        .then(response => response.json())
+        .then(data => buttons = data)
+        .catch(document.getElementById("question").innerHTML = "An error has occurred loading this page, please reload.")
+    fetch(`./json/${lang}/questions-${lang}.json`)
+        .then(response => response.json())
+        .then(data => load_questions(data))
+}
+
+function parse_langs(data){
+    let langs = []
+    for(let i=0; i < data.length; i++){
+        langs.push(data[i].id)
+    }
+    if (langs.some(element => element === window.location.search.substring(1))){
+        lang = window.location.search.substring(1)
+    } else {
+        lang = "en"
+    }
+    fetch(`./json/${lang}/ui-${lang}.json`)
+        .then(response => response.json())
+        .then(data => load_ui(data.quiz))
+}
+
+window.onload = () => fetch(`./json/langs.json`)
+    .then(response => response.json())
+    .then(data => parse_langs(data))
