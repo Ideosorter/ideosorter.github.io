a0d24a59492fb42fa64c752e21039b39c5103bd7
Aurélio A. Heckert <aurium@gmail.com>
2022-11-05 08:58:48 -0300

    Add loading animation

--- scripts/common.js
+++ scripts/common.js
@@ -2,4 +2,13 @@ import { loadL10n } from "./i18n.js"
 
 const currentPage = document.location.href.replace(/.*\/|(\.html|\?).*/g, '') || 'index'
 
-loadL10n().then(()=> import(`./page-${currentPage}.js`))
+const loadingEl = document.createElement('div')
+loadingEl.id = 'loading'
+document.body.appendChild(loadingEl)
+
+loadL10n()
+    .then(()=> import(`./page-${currentPage}.js`))
+    .then(()=> {
+        loadingEl.className = 'hide'
+        setTimeout(()=> loadingEl.remove(), 1000)
+    })
