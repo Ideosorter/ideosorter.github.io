7b6ffd6c76f0501a326c8ab2aba29ccc3445215a
TheGhostOfInky <theghostofinky@gmail.com>
2024-03-06 14:27:11 +0000

    Fixed race condition causing index to fail to load

--- scripts/page-index.js
+++ scripts/page-index.js
@@ -2,25 +2,29 @@ import questions from "./questions.js"
 
 const results = Object.values(questions)
     .flatMap(q => Object.values(q.results))   // Get all results in a one dim array
-    .filter((r, i, arr)=> arr.indexOf(r)===i) // Remove repeated items
+    .filter((r, i, arr) => arr.indexOf(r) === i) // Remove repeated items
 
 const langDropdown = document.getElementById("langDropdown")
 
-langDropdown.addEventListener("change", ()=> {
+langDropdown.addEventListener("change", () => {
     location.href = `index.html?${langDropdown.value}`
 })
 
 i18n.$l10nLangs.forEach(lang => {
     const selected = lang === i18n.$lang
-    langDropdown.innerHTML += `<option value="${lang}" ${selected&&"selected"}>${i18n.$l10nData[lang].lang_name}</option>`
+    langDropdown.innerHTML += `<option value="${lang}" ${selected && "selected"}>${i18n.$l10nData[lang].lang_name}</option>`
 })
 
+const textBody = document.getElementById("text_body")
+
+textBody.innerHTML = i18n.ui_index_text
 document.getElementById("title").innerHTML = i18n.ui_index_title
-document.getElementById("text_body").innerHTML = i18n.ui_index_text
 document.getElementById("startbutton").innerHTML = i18n.ui_index_start
 document.getElementById("treebutton").innerHTML = i18n.ui_index_tree
 document.getElementById("contacts").innerHTML = i18n.ui_index_contacts
 document.getElementById("contacts_info").innerHTML = i18n.ui_index_contact_info
 document.getElementById("startbutton").onclick = () => location.href = `quiz.html?${i18n.$lang}`
 document.getElementById("treebutton").onclick = () => location.href = `tree.html?${i18n.$lang}`
-document.getElementById("ideo_length").innerHTML = results.length
+textBody.onload = () => {
+    document.getElementById("ideo_length").innerHTML = results.length
+}
