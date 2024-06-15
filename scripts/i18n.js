b60025379d6ece55449a636e40499f453d099e5c
TheGhostOfInky <theghostofinky@gmail.com>
2024-06-15 20:08:58 +0100

    Added ukrainian translation by Matronium and worked around firefox bug related to proxies

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -2,18 +2,18 @@ const langRequested = window.location.search.substring(1).split('&')[0]
 const langEnforce = langRequested ? [langRequested] : null
 
 export async function loadL10n() {
-    const langs = ['cs', 'de', 'en', 'es', 'ko', 'pt', 'zhcn', 'zhtw', 'fr']
-    const l10nDataList = await Promise.all([
-        ...langs.map(lang =>
+    const langs = ['cs', 'de', 'en', 'es', 'ko', 'pt', 'zhcn', 'zhtw', 'fr', 'uk']
+    const [l10nDataList, i18nBuilderMod] = await Promise.all([
+        Promise.all(langs.map(lang =>
             fetch(`./scripts/l10n/${lang}.json`)
                 .then(res => res.json())
                 .catch(err => {
                     throw Error(`Fail to load ${lang} lang data.\n\n${err.message}`)
                 })
-        ),
+        )),
         import("https://unpkg.com/@aurium/i18n@0.4.1") // parallel loading
     ])
-    const i18nBuilder = l10nDataList.pop().default
+    const i18nBuilder = i18nBuilderMod.default
     const l10nData = {}
     langs.forEach((lang, i) => l10nData[lang] = l10nDataList[i])
     window.i18n = i18nBuilder(l10nData, langEnforce)
--- scripts/page-index.js
+++ scripts/page-index.js
@@ -10,6 +10,9 @@ langDropdown.addEventListener("change", () => {
     location.href = `index.html?${langDropdown.value}`
 })
 
+const i18n = window.i18n
+if (!i18n || !i18n.$l10nLangs.length) throw new Error("i18n module not loaded")
+
 i18n.$l10nLangs.forEach(lang => {
     const selected = lang === i18n.$lang
     langDropdown.innerHTML += `<option value="${lang}" ${selected && "selected"}>${i18n.$l10nData[lang].lang_name}</option>`
@@ -17,12 +20,12 @@ i18n.$l10nLangs.forEach(lang => {
 
 const textBody = document.getElementById("text_body")
 
-textBody.innerHTML = i18n.ui_index_text
-document.getElementById("title").innerHTML = i18n.ui_index_title
-document.getElementById("startbutton").innerHTML = i18n.ui_index_start
-document.getElementById("treebutton").innerHTML = i18n.ui_index_tree
-document.getElementById("contacts").innerHTML = i18n.ui_index_contacts
-document.getElementById("contacts_info").innerHTML = i18n.ui_index_contact_info
+textBody.innerHTML = [...i18n.ui_index_text].join("")
+document.getElementById("title").innerText = i18n.ui_index_title
+document.getElementById("startbutton").innerText = i18n.ui_index_start
+document.getElementById("treebutton").innerText = i18n.ui_index_tree
+document.getElementById("contacts").innerHTML = [...i18n.ui_index_contacts].join("")
+document.getElementById("contacts_info").innerHTML = [...i18n.ui_index_contact_info].join("")
 document.getElementById("startbutton").onclick = () => location.href = `quiz.html?${i18n.$lang}`
 document.getElementById("treebutton").onclick = () => location.href = `tree.html?${i18n.$lang}`
 textBody.onload = () => {
